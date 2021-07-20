f5274839f1e686ca62ed9d5105020a8167de8b68
realvint <victor.malukhin@gmail.com>
2021-07-21 00:39:17 +0300

    create models: user, category, test, question, answer

---
+++ db/migrate/20210720212151_create_tests.rb
@@ -0,0 +1,10 @@
+class CreateTests < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tests do |t|
+      t.string :title, null: false
+      t.integer :level, default: 0, null: false
+
+      t.timestamps
+    end
+  end
+end
