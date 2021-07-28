15bbcce9cf2b1e0a9f9e9110bff1af9982138d85
realvint <victor.malukhin@gmail.com>
2021-07-28 15:45:45 +0300

    create associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,13 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+
+  has_many :questions, dependent: :destroy
+  has_many :users, through: :user_tests
+  has_many :user_tests, dependent: :destroy
+
   def self.find_by_category(title)
-    joins('JOIN categories ON categories.id = tests.category_id')
+    joins(:category)
       .where(categories: { title: title })
       .order(title: :desc)
       .pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
+  has_many :tests, through: :user_tests
+  has_many :user_tests, dependent: :destroy
+
   def find_user_tests_by_level(level)
-    Test.where(level: level)
-        .joins('JOIN user_tests ON user_tests.test_id = tests.id')
-        .where(user_tests: { user_id: id })
+    tests.where(level: level)
   end
 end
