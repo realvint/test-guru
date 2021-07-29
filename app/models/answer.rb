8caba136d99ab7d97ed64c2e3922ebac35d74c46
realvint <victor.malukhin@gmail.com>
2021-07-29 18:19:22 +0300

    add validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,12 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  validates :title, presence: true
+  validate :answers_count, on: :create
+
+  private
+
+  def answers_count
+    errors.add(:question, 'У одного Вопроса может быть от 1-го до 4-х ответов.') if question.answers.count >= 4
+  end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -6,6 +6,12 @@ class Test < ApplicationRecord
   has_many :users, through: :user_tests
   has_many :user_tests, dependent: :destroy
 
+  validates :title, presence: true,
+                    uniqueness: { scope: :level }
+
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 },
+                    uniqueness: { scope: :title }
+
   def self.find_by_category(title)
     joins(:category)
       .where(categories: { title: title })
