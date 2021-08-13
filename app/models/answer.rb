a1110f1809d67275a5fffb9bb05f4ff8ff0b8aea
realvint <victor.malukhin@gmail.com>
2021-08-13 23:16:24 +0300

    add answers controller, views

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -9,6 +9,6 @@ class Answer < ApplicationRecord
   private
 
   def answers_count
-    errors.add(:question, 'У одного Вопроса может быть от 1-го до 4-х ответов.') if question.answers.count >= 4
+    errors.add(:title, 'У одного Вопроса может быть от 1-го до 4-х ответов.') if question.answers.count >= 4
   end
 end
