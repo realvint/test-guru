a4bc846630a8aa12aebc3088d9d20307bfb1ae1d
realvint <victor.malukhin@gmail.com>
2021-08-18 20:19:54 +0300

    tasks for lesson 10 done

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  skip_before_action :authenticate_user!
+
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to cookies[:current_url] || tests_path
+    else
+      flash[:alert] = 'Неправильный логин или пароль'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    cookies[:current_url] = nil
+    redirect_to root_path
+  end
+
+end
