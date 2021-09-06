de72b729467a593090efff2e37671422d116cbed
realvint <victor.malukhin@gmail.com>
2021-09-06 21:50:07 +0300

    refactor password confirming

--- app/javascript/packs/utilities/password_confirming.js
+++ app/javascript/packs/utilities/password_confirming.js
@@ -1,23 +1,6 @@
+import { PasswordConfirmation } from './PasswordConfirmation.js'
+
 document.addEventListener('turbolinks:load', () => {
-  const confirm_field = document.querySelector('#user_password_confirmation')
-
-  if (confirm_field == null)
-    return
-  else {
-    const wrongConfirmation = document.querySelector(".octicon-alert")
-    const successConfirmation = document.querySelector(".octicon-check")
-
-    confirm_field.addEventListener('change', event => {
-      const confirm = event.target.value
-      const password = document.querySelector('#user_password').value
-      if (confirm === password) {
-        successConfirmation.classList.remove('hide')
-        wrongConfirmation.classList.add('hide')
-      }
-      else {
-        successConfirmation.classList.add('hide')
-        wrongConfirmation.classList.remove('hide')
-      }
-    })
-  }
+  const form = document.getElementById('new_user')
+  if(form) new PasswordConfirmation(form)
 })
