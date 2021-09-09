feaa260236e9026a8d69cc102b6d116c2fa3f8a7
realvint <victor.malukhin@gmail.com>
2021-09-09 16:44:26 +0300

    add test_passages to question

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,7 @@ class Question < ApplicationRecord
 
   has_many :answers, dependent: :destroy
   has_many :gists, dependent: :destroy
+  has_many :test_passages, dependent: :destroy
 
   validates :title, presence: true
 end
