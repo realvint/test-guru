c37d3e00461d4210f5e325a94bfb484e9f96f26f
realvint <victor.malukhin@gmail.com>
2021-09-12 22:46:01 +0300

    add user_badges table

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,4 +1,7 @@
 class Badge < ApplicationRecord
 
   validates :title, presence: true
+
+  has_many :user_badges, dependent: :destroy
+  has_many :users, through: :user_badges
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_11_191340) do
+ActiveRecord::Schema.define(version: 2021_09_12_170758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,15 @@ ActiveRecord::Schema.define(version: 2021_09_11_191340) do
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
+  create_table "user_badges", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "badge_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["badge_id"], name: "index_user_badges_on_badge_id"
+    t.index ["user_id"], name: "index_user_badges_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email", default: "", null: false
@@ -116,4 +125,6 @@ ActiveRecord::Schema.define(version: 2021_09_11_191340) do
   add_foreign_key "test_passages", "users"
   add_foreign_key "tests", "categories"
   add_foreign_key "tests", "users", column: "author_id"
+  add_foreign_key "user_badges", "badges"
+  add_foreign_key "user_badges", "users"
 end
